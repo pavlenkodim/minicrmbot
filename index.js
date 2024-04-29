e4f4edcfc40f394d2e25fc8b79f3b8a1cbd9dd4f
pavlenkodim <pavlenkodim@mail.ru>
2024-04-29 16:38:12 +0500

    Add npm node-telegram-bot-api. Add function start, register. Add message descriptons file - bot_messages.json

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-const {TOKEN} = require('./bot_config.json')
+const { TOKEN } = require('./options/bot_config.json')
+const MESSAGE = require('./options/bot_messages.json')
+const TelegramBot = require('node-telegram-bot-api')
 
-console.log(TOKEN)
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+async function register(chatId) {
+    try {
+        bot.sendMessage(chatId, MESSAGE.CHANGE_DATE)
+        
+    } catch (error) {
+        console.warn(error)
+    }
+}
+
+function start() {
+    bot.setMyCommands([
+        {command: '/price', description: 'Прайс'},
+        {command: '/register', description: 'Записаться'},
+        {command: '/help', description: 'Помощь'}
+    ])
+
+    bot.on('message', msg => {
+        try {
+            const chatId = msg.chat.id;
+            const text = msg.text;
+
+            if (msg.entities[0].type === 'bot_command') {
+                switch (text) {
+                    case '/start':
+                        bot.sendMessage(chatId, MESSAGE.WELCOME)
+                        break;
+                    case '/help':
+                        bot.sendMessage(chatId, MESSAGE.HELP)
+                        break;
+                    case '/price':
+                        bot.sendMessage(chatId, MESSAGE.PRICE)
+                        break;
+                    case '/register':
+                        console.log(msg) //TODO: Delete after testing
+                        register(chatId)
+                        break;
+                    default:
+                        bot.sendMessage(chatId, MESSAGE.COMMAND_NOT_FOUND)
+                        break;
+                }
+            }
+
+        } catch (error) {
+            console.warn(error)
+        }
+    })
+}
+
+start()
